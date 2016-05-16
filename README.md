2883d35671c5cb8e80cd4b57a5a7cad6ba89ab3d
Zutatensuppe <oo.para@gmail.com>
2016-05-16 15:44:43 +0200

    context menu instead of buttons in main window

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ The information that is currently shown in the tool are as follows:
 
 **Main window**  
 ![Main tool window](https://github.com/Zutatensuppe/DiabloInterface/raw/master/docs/assets/img/mainWindow.png)
+![Main tool window, v0.2.1](https://github.com/Zutatensuppe/DiabloInterface/raw/master/docs/assets/img/mainWindow-v0.2.1.png)
 
 **Settings window**  
 ![Settings tool window](https://github.com/Zutatensuppe/DiabloInterface/raw/master/docs/assets/img/settingsWindow.png)
@@ -34,6 +35,8 @@ If you just want to use the tool and not mess with source code, use any of the b
 
 ### Current Version:
 Current version allows automatic splits in connection with a split tool like Livesplit. You need to setup the same splits and hotkeys as in your split tool (hotkey in DiabloInterface = Start/Split hotkey in split tool). There is no timer integrated directly into this tool (yet). Please note that automatic splits will only work if you start a new character while the tool is running.
+
+[DiabloInterface_Autosplits_v0.2.1_.NET4.5.2.exe](https://github.com/Zutatensuppe/DiabloInterface/raw/master/builds/1.14B/DiabloInterface_Autosplits_v0.2.1_.NET4.5.2.exe) (Context Menu instead of Buttons, D2 Patch 1.14B, compiled for .NET 4.5.2)
 
 [DiabloInterface_Autosplits_v0.2_.NET4.5.2.exe](https://github.com/Zutatensuppe/DiabloInterface/raw/master/builds/1.14B/DiabloInterface_Autosplits_v0.2_.NET4.5.2.exe) (D2 Patch 1.14B, compiled for .NET 4.5.2)
 
--- src/DiabloInterface/MainWindow.Designer.cs
+++ src/DiabloInterface/MainWindow.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
-            this.button1 = new System.Windows.Forms.Button();
             this.lvlLabel = new System.Windows.Forms.Label();
             this.nameLabel = new System.Windows.Forms.Label();
             this.strengthLabel = new System.Windows.Forms.Label();
@@ -42,25 +42,18 @@
             this.poisonResLabel = new System.Windows.Forms.Label();
             this.goldLabel = new System.Windows.Forms.Label();
             this.deathsLabel = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenu.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 122);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(61, 25);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Exit";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // lvlLabel
             // 
             this.lvlLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.lvlLabel.ForeColor = System.Drawing.Color.White;
-            this.lvlLabel.Location = new System.Drawing.Point(244, 63);
+            this.lvlLabel.Location = new System.Drawing.Point(159, 63);
             this.lvlLabel.Name = "lvlLabel";
             this.lvlLabel.Size = new System.Drawing.Size(80, 16);
             this.lvlLabel.TabIndex = 2;
@@ -70,9 +63,9 @@
             // 
             this.nameLabel.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.nameLabel.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.nameLabel.Location = new System.Drawing.Point(97, 9);
+            this.nameLabel.Location = new System.Drawing.Point(12, 9);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(222, 33);
+            this.nameLabel.Size = new System.Drawing.Size(260, 33);
             this.nameLabel.TabIndex = 6;
             this.nameLabel.Text = "Char Name";
             // 
@@ -81,7 +74,7 @@
             this.strengthLabel.AutoSize = true;
             this.strengthLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.strengthLabel.ForeColor = System.Drawing.Color.Coral;
-            this.strengthLabel.Location = new System.Drawing.Point(100, 84);
+            this.strengthLabel.Location = new System.Drawing.Point(15, 84);
             this.strengthLabel.Name = "strengthLabel";
             this.strengthLabel.Size = new System.Drawing.Size(56, 16);
             this.strengthLabel.TabIndex = 7;
@@ -92,7 +85,7 @@
             this.dexterityLabel.AutoSize = true;
             this.dexterityLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.dexterityLabel.ForeColor = System.Drawing.Color.Coral;
-            this.dexterityLabel.Location = new System.Drawing.Point(100, 100);
+            this.dexterityLabel.Location = new System.Drawing.Point(15, 100);
             this.dexterityLabel.Name = "dexterityLabel";
             this.dexterityLabel.Size = new System.Drawing.Size(56, 16);
             this.dexterityLabel.TabIndex = 8;
@@ -103,7 +96,7 @@
             this.vitalityLabel.AutoSize = true;
             this.vitalityLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.vitalityLabel.ForeColor = System.Drawing.Color.Coral;
-            this.vitalityLabel.Location = new System.Drawing.Point(100, 116);
+            this.vitalityLabel.Location = new System.Drawing.Point(15, 116);
             this.vitalityLabel.Name = "vitalityLabel";
             this.vitalityLabel.Size = new System.Drawing.Size(56, 16);
             this.vitalityLabel.TabIndex = 10;
@@ -114,7 +107,7 @@
             this.energyLabel.AutoSize = true;
             this.energyLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.energyLabel.ForeColor = System.Drawing.Color.Coral;
-            this.energyLabel.Location = new System.Drawing.Point(100, 132);
+            this.energyLabel.Location = new System.Drawing.Point(15, 132);
             this.energyLabel.Name = "energyLabel";
             this.energyLabel.Size = new System.Drawing.Size(56, 16);
             this.energyLabel.TabIndex = 11;
@@ -125,7 +118,7 @@
             this.fireResLabel.AutoSize = true;
             this.fireResLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.fireResLabel.ForeColor = System.Drawing.Color.Red;
-            this.fireResLabel.Location = new System.Drawing.Point(244, 84);
+            this.fireResLabel.Location = new System.Drawing.Point(159, 84);
             this.fireResLabel.Name = "fireResLabel";
             this.fireResLabel.Size = new System.Drawing.Size(64, 16);
             this.fireResLabel.TabIndex = 12;
@@ -136,7 +129,7 @@
             this.coldResLabel.AutoSize = true;
             this.coldResLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.coldResLabel.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.coldResLabel.Location = new System.Drawing.Point(244, 100);
+            this.coldResLabel.Location = new System.Drawing.Point(159, 100);
             this.coldResLabel.Name = "coldResLabel";
             this.coldResLabel.Size = new System.Drawing.Size(64, 16);
             this.coldResLabel.TabIndex = 13;
@@ -147,7 +140,7 @@
             this.lightningResLabel.AutoSize = true;
             this.lightningResLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.lightningResLabel.ForeColor = System.Drawing.Color.Yellow;
-            this.lightningResLabel.Location = new System.Drawing.Point(244, 116);
+            this.lightningResLabel.Location = new System.Drawing.Point(159, 116);
             this.lightningResLabel.Name = "lightningResLabel";
             this.lightningResLabel.Size = new System.Drawing.Size(64, 16);
             this.lightningResLabel.TabIndex = 14;
@@ -158,7 +151,7 @@
             this.poisonResLabel.AutoSize = true;
             this.poisonResLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.poisonResLabel.ForeColor = System.Drawing.Color.YellowGreen;
-            this.poisonResLabel.Location = new System.Drawing.Point(244, 132);
+            this.poisonResLabel.Location = new System.Drawing.Point(159, 132);
             this.poisonResLabel.Name = "poisonResLabel";
             this.poisonResLabel.Size = new System.Drawing.Size(64, 16);
             this.poisonResLabel.TabIndex = 15;
@@ -169,7 +162,7 @@
             this.goldLabel.AutoSize = true;
             this.goldLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.goldLabel.ForeColor = System.Drawing.Color.Gold;
-            this.goldLabel.Location = new System.Drawing.Point(100, 63);
+            this.goldLabel.Location = new System.Drawing.Point(15, 63);
             this.goldLabel.Name = "goldLabel";
             this.goldLabel.Size = new System.Drawing.Size(64, 16);
             this.goldLabel.TabIndex = 16;
@@ -180,40 +173,50 @@
             this.deathsLabel.AutoSize = true;
             this.deathsLabel.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.deathsLabel.ForeColor = System.Drawing.Color.Snow;
-            this.deathsLabel.Location = new System.Drawing.Point(100, 42);
+            this.deathsLabel.Location = new System.Drawing.Point(15, 42);
             this.deathsLabel.Name = "deathsLabel";
             this.deathsLabel.Size = new System.Drawing.Size(80, 16);
             this.deathsLabel.TabIndex = 17;
             this.deathsLabel.Text = "DEATHS: -";
             // 
-            // button2
+            // contextMenu
             // 
-            this.button2.Location = new System.Drawing.Point(12, 93);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(61, 23);
-            this.button2.TabIndex = 18;
-            this.button2.Text = "Reset";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.ShowImageMargin = false;
+            this.contextMenu.Size = new System.Drawing.Size(92, 70);
             // 
-            // button3
+            // settingsToolStripMenuItem
             // 
-            this.button3.Location = new System.Drawing.Point(12, 63);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(61, 24);
-            this.button3.TabIndex = 20;
-            this.button3.Text = "Settings";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(334, 161);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.ContextMenuStrip = this.contextMenu;
             this.Controls.Add(this.deathsLabel);
             this.Controls.Add(this.goldLabel);
             this.Controls.Add(this.poisonResLabel);
@@ -226,22 +229,21 @@
             this.Controls.Add(this.strengthLabel);
             this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.lvlLabel);
-            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainWindow";
-            this.Text = "DiabloInterface";
+            this.Text = "DiabloInterface v0.2.1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.contextMenu.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lvlLabel;
         private System.Windows.Forms.Label nameLabel;
         private System.Windows.Forms.Label strengthLabel;
@@ -254,8 +256,10 @@
         private System.Windows.Forms.Label poisonResLabel;
         private System.Windows.Forms.Label goldLabel;
         private System.Windows.Forms.Label deathsLabel;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
