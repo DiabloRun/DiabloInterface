ab300494038c76886100ba55978ca476d936ce1d
Zutatensuppe <oo.para@gmail.com>
2017-12-16 13:22:04 +0100

    minor refactorings/typo fixes (additional class files)

---
+++ src/D2Reader/Models/GameVersion.cs
@@ -0,0 +1,12 @@
+namespace Zutatensuppe.D2Reader.Models
+{
+    public class GameVersion
+    {
+        public const string V113C = "1, 0, 13, 60";
+        public const string V113D = "1, 0, 13, 64";
+
+        public const string V114B = "1.14.1.68";
+        public const string V114C = "1.14.2.70";
+        public const string V114D = "1.14.3.71";
+    }
+}
---
+++ src/DiabloInterface.Core/Utility.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Zutatensuppe.DiabloInterface.Core
+{
+    public class Utility
+    {
+        public static T Clamp<T>(T value, T min, T max) where T : IComparable<T>
+        {
+            if (value.CompareTo(min) < 0)
+                return min;
+
+            if (value.CompareTo(max) > 0)
+                return max;
+
+            return value;
+        }
+    }
+}
