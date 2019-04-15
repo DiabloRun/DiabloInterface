17b895a735b15ee3a9d89179b4a2cdb3405b9dec
Zutatensuppe <oo.para@gmail.com>
2019-04-15 23:34:41 +0200

    Version Bump 0.4.7

--- src/DiabloInterface/Properties/AssemblyInfo.cs
+++ src/DiabloInterface/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("25bb3310-9bd7-4234-af7d-9f7fb6870a20")]
 
-[assembly: AssemblyVersion("0.4.6")]
-[assembly: AssemblyInformationalVersion("0.4.6")]
+[assembly: AssemblyVersion("0.4.7")]
+[assembly: AssemblyInformationalVersion("0.4.7")]
