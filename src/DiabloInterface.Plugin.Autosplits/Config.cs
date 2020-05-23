b4c73345aa8459e0845ddfaca9af4f082ebcc195
Zutatensuppe <oo.para@gmail.com>
2020-05-23 22:46:39 +0200

    add option to split for existing chars (implement #58)

--- src/DiabloInterface.Plugin.Autosplits/Config.cs
+++ src/DiabloInterface.Plugin.Autosplits/Config.cs
@@ -7,6 +7,7 @@ namespace Zutatensuppe.DiabloInterface.Plugin.Autosplits
     internal class Config : IPluginConfig
     {
         public bool Enabled = false;
+        public bool EnabledForExistingChars = false;
         public Hotkey Hotkey = new Hotkey();
         public Hotkey ResetHotkey = new Hotkey();
         public List<AutoSplit> Splits = new List<AutoSplit>();
