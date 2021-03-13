a44eef5f861941f05a822c3416ddd3c08acca5e4
Zutatensuppe <oo.para@gmail.com>
2021-03-13 15:22:16 +0100

    use specific assert functions instead of equals

--- src/DiabloInterface.Plugin.HttpClient.Test/DiffUtilTest.cs
+++ src/DiabloInterface.Plugin.HttpClient.Test/DiffUtilTest.cs
@@ -11,32 +11,32 @@ namespace DiabloInterface.Plugin.HttpClient.Test
         [TestMethod]
         public void TestListsEqualSimple()
         {
-            Assert.AreEqual(true, DiffUtil.ListsEqual(
+            Assert.IsTrue(DiffUtil.ListsEqual(
                 (List<int>)null,
                 null
             ), "equal (both null)");
 
-            Assert.AreEqual(true, DiffUtil.ListsEqual(
+            Assert.IsTrue(DiffUtil.ListsEqual(
                 new List<int> { 1, 2, 3 },
                 new List<int> { 1, 2, 3 }
             ), "equal (values)");
 
-            Assert.AreEqual(false, DiffUtil.ListsEqual(
+            Assert.IsFalse(DiffUtil.ListsEqual(
                 new List<int> { 1, 2, 3 },
                 null
             ), "not equal (second null)");
 
-            Assert.AreEqual(false, DiffUtil.ListsEqual(
+            Assert.IsFalse(DiffUtil.ListsEqual(
                 null,
                 new List<int> { 1, 2, 3 }
             ), "not equal (first null)");
 
-            Assert.AreEqual(false, DiffUtil.ListsEqual(
+            Assert.IsFalse(DiffUtil.ListsEqual(
                 new List<int> { 1, 2, 3 },
                 new List<int> { 1, 2, 4 }
             ), "not equal (values)");
 
-            Assert.AreEqual(false, DiffUtil.ListsEqual(
+            Assert.IsFalse(DiffUtil.ListsEqual(
                 new List<int> { 1, 2, 3 },
                 new List<int> { 1, 3, 2 }
             ), "not equal (order)");
@@ -51,37 +51,37 @@ namespace DiabloInterface.Plugin.HttpClient.Test
             var z2 = new MockType{ Some = 108, Thing = "Hello_Z" };
             var a = new MockType{ Some = 108, Thing = "Hello_A" };
 
-            Assert.AreEqual(true, DiffUtil.ListsEqual(
+            Assert.IsTrue(DiffUtil.ListsEqual(
                 (List<object>)null,
                 null
             ), "equal (null)");
 
-            Assert.AreEqual(true, DiffUtil.ListsEqual(
+            Assert.IsTrue(DiffUtil.ListsEqual(
                 new List<object> { x, y, z },
                 new List<object> { x, y, z }
             ), "equal (values)");
 
-            Assert.AreEqual(false, DiffUtil.ListsEqual(
+            Assert.IsFalse(DiffUtil.ListsEqual(
                 null,
                 new List<object> { x, y, z }
             ), "not equal (first null)");
 
-            Assert.AreEqual(false, DiffUtil.ListsEqual(
+            Assert.IsFalse(DiffUtil.ListsEqual(
                 new List<object> { x, y, z },
                 null
             ), "not equal (second null)");
 
-            Assert.AreEqual(false, DiffUtil.ListsEqual(
+            Assert.IsFalse(DiffUtil.ListsEqual(
                 new List<object> { x, y, z },
                 new List<object> { x, y, a }
             ), "not equal (values)");
 
-            Assert.AreEqual(false, DiffUtil.ListsEqual(
+            Assert.IsFalse(DiffUtil.ListsEqual(
                 new List<object> { x, y, z },
                 new List<object> { x, z, y }
             ), "not equal (order)");
 
-            Assert.AreEqual(false, DiffUtil.ListsEqual(
+            Assert.IsFalse(DiffUtil.ListsEqual(
                 new List<object> { x, y, z },
                 new List<object> { x, y, z2 }
             ), "not equal (same value, but not same reference)");
@@ -90,33 +90,33 @@ namespace DiabloInterface.Plugin.HttpClient.Test
         [TestMethod]
         public void TestObjectsEqual()
         {
-            Assert.AreEqual(false, DiffUtil.ObjectsEqual(
+            Assert.IsFalse(DiffUtil.ObjectsEqual(
                 null,
                 new MockType { Some = 108, Thing = "Hello_Z" }
             ), "first is null");
 
-            Assert.AreEqual(false, DiffUtil.ObjectsEqual(
+            Assert.IsFalse(DiffUtil.ObjectsEqual(
                 new MockType { Some = 108, Thing = "Hello_Z" },
                 null
             ), "second is null");
 
-            Assert.AreEqual(true, DiffUtil.ObjectsEqual(
+            Assert.IsTrue(DiffUtil.ObjectsEqual(
                 null,
                 null
             ), "both null");
 
-            Assert.AreEqual(false, DiffUtil.ObjectsEqual(
+            Assert.IsFalse(DiffUtil.ObjectsEqual(
                 new MockType { Some = 108, Thing = "Hello_Z" },
                 new MockType { Some = 108, Thing = "Hello_Z" }
             ), "same value, but not same ref");
 
             var obj = new MockType { Some = 108, Thing = "Hello_Z" };
-            Assert.AreEqual(true, DiffUtil.ObjectsEqual(
+            Assert.IsTrue(DiffUtil.ObjectsEqual(
                 obj,
                 obj
             ), "same value and same ref");
 
-            Assert.AreEqual(true, DiffUtil.ObjectsEqual(
+            Assert.IsTrue(DiffUtil.ObjectsEqual(
                 new { Some = 108, Thing = "Hello_Z" },
                 new { Some = 108, Thing = "Hello_Z" }
             ), "same value and (anonymous obj that doesnt use ref)");
@@ -131,15 +131,15 @@ namespace DiabloInterface.Plugin.HttpClient.Test
 
             // nothing old, nothing new (null version)
             res = DiffUtil.ItemsDiff(curr, prev);
-            Assert.AreEqual(null, res.Item1);
-            Assert.AreEqual(null, res.Item2);
+            Assert.IsNull(res.Item1);
+            Assert.IsNull(res.Item2);
 
             // nothing old, nothing new (empty list version)
             prev = new List<ItemInfo>();
             curr = new List<ItemInfo>();
             res = DiffUtil.ItemsDiff(curr, prev);
-            Assert.AreEqual(null, res.Item1);
-            Assert.AreEqual(null, res.Item2);
+            Assert.IsNull(res.Item1);
+            Assert.IsNull(res.Item2);
 
             // nothing old, one new
             // -> nothing removed, one added
@@ -149,8 +149,8 @@ namespace DiabloInterface.Plugin.HttpClient.Test
             };
             res = DiffUtil.ItemsDiff(curr, prev);
             Assert.AreEqual(1, res.Item1.Count);
-            Assert.AreEqual(null, res.Item2);
-            Assert.AreEqual(true, ItemInfo.AreEqual(curr[0], res.Item1[0]));
+            Assert.IsNull(res.Item2);
+            Assert.IsTrue(ItemInfo.AreEqual(curr[0], res.Item1[0]));
 
             // one old, one new
             // -> nothing removed, one added
@@ -165,8 +165,8 @@ namespace DiabloInterface.Plugin.HttpClient.Test
             };
             res = DiffUtil.ItemsDiff(curr, prev);
             Assert.AreEqual(1, res.Item1.Count);
-            Assert.AreEqual(null, res.Item2);
-            Assert.AreEqual(true, ItemInfo.AreEqual(curr[1], res.Item1[0]));
+            Assert.IsNull(res.Item2);
+            Assert.IsTrue(ItemInfo.AreEqual(curr[1], res.Item1[0]));
 
             // one old, one new
             // -> one removed, one added
@@ -181,8 +181,8 @@ namespace DiabloInterface.Plugin.HttpClient.Test
             res = DiffUtil.ItemsDiff(curr, prev);
             Assert.AreEqual(1, res.Item1.Count);
             Assert.AreEqual(1, res.Item2.Count);
-            Assert.AreEqual(true, ItemInfo.AreEqual(curr[0], res.Item1[0]));
-            Assert.AreEqual(true, ItemInfo.AreEqual(prev[0], res.Item2[0]));
+            Assert.IsTrue(ItemInfo.AreEqual(curr[0], res.Item1[0]));
+            Assert.IsTrue(ItemInfo.AreEqual(prev[0], res.Item2[0]));
 
             // one old, nothing new
             // -> one removed, nothing added
@@ -194,9 +194,9 @@ namespace DiabloInterface.Plugin.HttpClient.Test
             {
             };
             res = DiffUtil.ItemsDiff(curr, prev);
-            Assert.AreEqual(null, res.Item1);
+            Assert.IsNull(res.Item1);
             Assert.AreEqual(1, res.Item2.Count);
-            Assert.AreEqual(true, ItemInfo.AreEqual(prev[0], res.Item2[0]));
+            Assert.IsTrue(ItemInfo.AreEqual(prev[0], res.Item2[0]));
         }
 
         [TestMethod]
